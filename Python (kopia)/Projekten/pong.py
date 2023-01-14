3b602bd74f792be16dfc5515ea4f1f951a3f773f
GGisMee <gustav.gamstedt@outlook.com>
2023-01-14 20:34:42 +0100

    small changes

--- Python (kopia)/Projekten/pong.py
+++ Python (kopia)/Projekten/pong.py
@@ -10,9 +10,9 @@ status = False
 class ballC:
     def __init__(self, speed):
         # early config
-        self.posx = 100
-        self.posy = 300
-        self.direction = 0
+        self.dirx = 100
+        self.diry = 300
+        self.direction = 179
         self.speed = speed
         
 
@@ -27,9 +27,11 @@ def started(status):
     ballinfo = ballC(speed)
 
 
-    canvas.move(ball, ballinfo.posx, ballinfo.posy)
+    canvas.move(ball, ballinfo.dirx, ballinfo.diry)
     while status == True:
         
+        Vx = round(sin(radians(ballinfo.direction)),2)
+        print(Vx, ballinfo.speed, ballinfo.direction)
 
         window.update()
         
@@ -50,7 +52,7 @@ speedEnt.pack()
 canvas = Canvas(window, width=600, height=600, bg="#f0f0f0")
 
 ball = canvas.create_oval(0,0, 15, 15, fill="black")
-ballinfo = "1"
+ballinfo = "ballinfo"
 
 canvas.pack()
 window.mainloop()
