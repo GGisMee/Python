c9509e167c4b140f52b7f2a5573ab76ebcdcc809
GGisMee <gustav.gamstedt@outlook.com>
2023-01-19 21:27:51 +0100

    forts

--- Python (kopia)/Code_storage/Vidare inlärning/PIL/2_manipulation.py
+++ Python (kopia)/Code_storage/Vidare inlärning/PIL/2_manipulation.py
@@ -4,3 +4,18 @@ from PIL import Image
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+image = Image.open("pic.jpg")
+
+if (inp := int(input())) == 1:
+    # image rotation                #expand = True för att expandera fottot vid rotering
+    rotated_image = image.rotate(10, expand = True) # sker motklocks
+    rotated_image.show()
+
+    # andra är
+    # fillcolor, för färg i utkant
+    # 
+
+if inp == 2:
+    pass
+else:
+    print("error. "+ str(inp))
