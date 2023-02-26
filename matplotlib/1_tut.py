0bd9ecd206a494fdf32e2edbad8bb8dea27f6f9f
Gustav Gamstedt <gustav.gamstedt@outlook.com>
2023-02-26 21:04:43 +0000

    matplotlib strt

---
+++ matplotlib/1_tut.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,2,3])
+y = np.array([2,4,6])
+plt.plot(x,y)
+plt.title("Our first graph")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
