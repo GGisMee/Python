dcd8d0f74849fee9e89618623a1405d6e96c8151
GGisMee <gustav.gamstedt@outlook.com>
2023-03-07 21:27:17 +0100

    some sql and some pandas

--- Python (kopia)/Projekten/prj_snake/pd.py
+++ Python (kopia)/Projekten/prj_snake/pd.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-df = pd.read_csv('Python (kopia)\Projekten\prj_snake\storage.csv')
+df = pd.read_csv('Python (kopia)/Projekten/prj_snake/storage.csv')
 print(df["#"])
 max = int(df['#'].max())
-new_row = pd.DataFrame({'#':[max+1], 'length':[2], 'speed':[3]})
+new_row = pd.DataFrame({'#':[max+1], 'length':[2], 'time':[3]})
 print(new_row)
 df = pd.concat([df,new_row], ignore_index=False, axis=0, join='outer')
 print(df)
@@ -12,4 +12,4 @@ try:
     df = df.drop(columns=["Unnamed: 0"])
 except KeyError:
     pass
-df.to_csv('Python (kopia)\Projekten\prj_snake\storage.csv')
+df.to_csv('Python (kopia)/Projekten/prj_snake/storage.csv')
--- sql/tutorial.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE student (
-    student_id INT PRIMARY KEY /*Primary key gör att är primär*/
-    name VARCHAR(20) /*VARCHAR = STR, 20 för antalet */
-
-);
