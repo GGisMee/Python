66682ba1c131d0a6563c9caa9e14d6a5397a898e
GGisMee <gustav.gamstedt@outlook.com>
2023-04-27 17:29:44 +0200

    js

---
+++ Js/full tut/7_math/main.js
@@ -0,0 +1,23 @@
+let x = 3.1415;
+let y = -2;
+let z = 9;
+let minimum;
+let maximum;
+
+//x = Math.round(x)
+//x = Math.ceil(x) // alltid upprundning
+//x = Math.floor(x) // alltid nedrundning
+//x = Math.pow(x, 2) // upphöjt i andra nummret alltså 2 i detta fallet
+//x = x ** 6 // samma
+// x = Math.sqrt(x)
+//y = Math.abs(y)
+maximum = Math.max(x, y, z)
+minimum = Math.min(x, y, z)
+Pi = Math.PI
+
+
+console.log("X:", x)
+console.log("Y:", y)
+console.log("Max:", maximum)
+console.log("Min:", minimum)
+console.log("PI:", Pi)
---
+++ Js/vue full tut/vue.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="app">
+        <h1>vue here</h1>
+    </div>
+</template>
