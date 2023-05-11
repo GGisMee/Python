7f3aa2718284ccccd1fd371231544d53a2e5b2e8
GGisMee <gustav.gamstedt@outlook.com>
2023-05-11 21:36:39 +0200

    f c++

--- Arduino_C++/Hemma_öva/5_typedef.cpp
+++ Arduino_C++/Hemma_öva/5_typedef.cpp
@@ -5,11 +5,18 @@
 // förkortning
 // typedef std::vector<std::pair<std::string, int>> pairlist_t;
 
-typedef std::string str;
+typedef std::string text_t;
+typedef int number_t;
+
+// annan metod
+
+// using text_t = std::string;
+// using number_t = int;
 
 int main(){
-    str name = "Gustav";
-    std::cout<<name;
+    text_t name = "Gustav";
+    number_t age = 16;
+    std::cout<<name<<" thats "<< age;
 
 
     return 0;
