031423634cd343e922813d9524cf3482abc76150
GGisMee <gustav.gamstedt@outlook.com>
2023-07-11 16:24:35 +0200

    början på rekord

--- Python/Projekten/tkinter/tetris/record_frame.py
+++ Python/Projekten/tkinter/tetris/record_frame.py
@@ -1,7 +1,18 @@
 import tkinter as tk
+from datetime import datetime 
+import pandas as pd
+import sys
+import numpy as np
+
+df = pd.read_csv(f"{sys.path[0]}/records.csv",index_col="ID")
+print(df.sort_values(df["Score"]))
+exit()
 
 window = tk.Tk()
-
+now = datetime.now()
+today = now.strftime("%Y-%m-%d")
+print(today)
+exit()
 
 
 window.mainloop()
