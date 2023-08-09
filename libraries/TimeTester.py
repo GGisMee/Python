880c28e8a6f50d35d5df796fae8b0d08a5e4538a
GGisMee <gustav.gamstedt@outlook.com>
2023-08-09 19:17:58 +0200

    deleted to finalize the repositioning

--- libraries/TimeTester.py
+++
@@ -1,7 +0,0 @@
-from time import time
-def timeit(func, times):
-    start = time()
-    for i in range(times):
-        func()
-    return time() - start 
-
--- libraries/input_to_type.py
+++
@@ -1,6 +0,0 @@
-# main float,int
-def input_f():
-    try:
-        return int(input("Enter number: "))
-    except ValueError:
-        input_f()
