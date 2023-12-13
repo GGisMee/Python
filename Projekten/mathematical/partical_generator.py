c5e252b6ba0d22b76dd6abd872a11d1f080a93eb
GGisMee <gustav.gamstedt@outlook.com>
2023-12-13 09:31:59 +0100

    pg

---
+++ Projekten/mathematical/partical_generator.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class particle:
+    def __init__(self, x:bool,y:bool):
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.ax = 0
+        self.ay = 0
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+    
+
+class partical_container:
+    def __init__(self, num_par:int) -> None:
+        """Creates lots of particles on an xy-plane with coordinates from 0-1
+        
+        Variables:
+            num_par: int = nummer  n"""
+        self.num_par = num_par
+        self.par_list = []
+        for i in range(num_par):
+            self.par_list.append(particle)
+
+    def 
