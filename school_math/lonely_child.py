6f9544353883089fb40dded832a9b2ce39806f16
GGisMee <gustav.gamstedt@outlook.com>
2024-02-22 17:38:58 +0100

    bug fixes, still something unusuall though

--- school_math/lonely_child.py
+++ school_math/lonely_child.py
@@ -13,29 +13,35 @@ class Child:
         return sqrt((self.x+child.x)**2+(self.y+child.y)**2)
 
     def throw(self):
-        print('run')
+        #print('run')
         distanses = []
         for child in children:
             if child == self:
-                print(child)
+                distanses.append([2, child])
                 continue
             distanses.append([self.getDistans(child), child])
         chosen = np.argmin(np.array(distanses)[:,0])
-        print(children[chosen])
-        print()
+        for i, child in enumerate(children): 
+            if child == distanses[chosen][1]:
+                children[i].got_ball = True
+                break
+def get_random():
+    random_1 = random()
+    random_2 = random()
+    if random_1 != random_2:
+        return random_1, random_2
+    return get_random()
+results = []
+iterations = 1000
+for i in range(iterations):
+    children = []
+    for i in range(3):
+        random1, random2 = get_random()
+        child = Child(random1, random2, i)
+        children.append(child)
+    for child in children:
+        child.throw()
+    results.append(np.all([child.got_ball for child in children]))
+print(np.sum(results))
 
-children = []
-
-for i in range(2):
-    child = Child(random(), random(), i)
-    children.append(child)
-for child in children:
-    child.throw()
-for child in children:
-    if not (child.got_ball):
-        print('False')
-        exit()
-    else:
-        print('True')
-        exit()
 
