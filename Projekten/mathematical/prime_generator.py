183f2290c4e75ac17332d510b44eec62991df38b
GGisMee <gustav.gamstedt@outlook.com>
2024-04-10 21:11:53 +0200

    hmm

--- Projekten/mathematical/prime_generator.py
+++ Projekten/mathematical/prime_generator.py
@@ -11,4 +11,17 @@ def get_primes(end):
     except IndexError:
         return primes
 
-primes = get_primes(400000)
+primes200 = get_primes(200)
+print(primes200, len(primes200))
+new_primes = []
+length = 0
+for prime in primes200:
+    inner_new_primes = []
+    for prime2 in primes200:
+        if prime*prime2 > 200:
+            primes200 = primes200[1:]
+            break
+        inner_new_primes.append(prime2)
+    new_primes.append(inner_new_primes)
+    length += len(inner_new_primes)
+print(new_primes ,length)
---
+++ Projekten/tkinter/choices_and_wishes/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tkinter as tk
+numChoices: int
+Classes: int
+
+
+window = tk.Tk()
+
+MFrame = tk.Frame(window)
+MFrame.pack()
+
+
+
+
+
+window.mainloop()
