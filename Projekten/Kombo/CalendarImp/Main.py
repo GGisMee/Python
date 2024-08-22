10c3f10a7dba74f7afd658113547be30ec35b6a3
GGisMee <gustav.gamstedt@outlook.com>
2024-08-22 13:37:29 +0200

    fixing, but problem with line elements

--- Projekten/Kombo/CalendarImp/Main.py
+++ Projekten/Kombo/CalendarImp/Main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 dir = path[0]
 
-startWeek: int = 35
+startWeek: int = 37
 numWeeks: int = 3
 
 def start(playwright):
@@ -25,7 +25,12 @@ def scrape_calendar(page, dir):
 
     # Select all text elements on the page and convert them into a numpy array
     text_elements = np.array(page.query_selector_all('text'))
+    page.wait_for_selector('line')
+
     line_elements = np.array(page.query_selector_all('line'))
+    if len(line_elements == 0):
+        print("No line elements found, please retry again")
+        exit()
     leftXOutline = int(line_elements[0].get_attribute("x2"))
     rightXOutline = int(line_elements[-1].get_attribute("x1"))
     Step = (rightXOutline-leftXOutline)/5
@@ -38,7 +43,6 @@ def scrape_calendar(page, dir):
 
     text_elements = text_elements[index+1:]
 
-    print(1)
     # # Removes empty elements
     text_elements = np.array([elem for elem in text_elements if elem.text_content()])
     
@@ -50,35 +54,26 @@ def scrape_calendar(page, dir):
         minX = Step * i + leftXOutline #! Skulle behöva göra den här dynamisk eftersom den ändras efter device.
         maxX = Step * (i + 1) + leftXOutline
         # Add elements to the current week list if their x-coordinate falls within the days range
-        if i == 3:
-            pass
-            #print(minX, maxX)
         for el in text_elements:
             if minX < np.int16(el.get_attribute("x")) < maxX:
-                if i == 3:
-                    pass
-                    
                 WeekList.append(el)
                 
         DevidedList.append(WeekList)
     
-    print(2)
+    TotalWeekList = []
     for WeekList in DevidedList:
         WeekList = np.array(WeekList)
         Dates = np.array([el for el in WeekList if ":" in el.text_content()])
         YDates = np.array([np.int16(el.get_attribute("y")) for el in Dates])
-        #IndexSortedYDates = np.argsort(YDates) #! problem med argsort då den förstör när två aktiviteter överlappar.
 
 
-        print([el.text_content() for el in Dates])
-        #YDates = YDates[IndexSortedYDates]
-        #Dates = Dates[IndexSortedYDates]
         WeekListNoDates = WeekList[~np.isin(WeekList, Dates)]
         
         XDates = np.array([np.int16(el.get_attribute("x")) for el in Dates])
 
 
         TextDates = np.array([el.text_content() for el in Dates])
+
         # I pair the dates up in two so that they are split into when they happen.
         PairedTextDates = [TextDates[i:i+2] for i in range(0, len(TextDates), 2)]
         PairedYDates = list([YDates[i:i+2] for i in range(0, len(YDates), 2)])
@@ -89,13 +84,23 @@ def scrape_calendar(page, dir):
         
         IndexList = [next((i for i, (start, end) in enumerate(PairedYDates) if start <= element <= end), None) for element in TextY]
         StructuredLectures = np.array(PairedTextDates).tolist()
-        print(3)
         for TextIndex, DatesIndex in enumerate(IndexList):
             if Text[TextIndex][0:3] == Text[TextIndex][3:6]:
-                Text[TextIndex][0:3]
+                Text[TextIndex] = Text[TextIndex][3:]
             StructuredLectures[DatesIndex].append(Text[TextIndex])
-        print(StructuredLectures)
-        #print(StructuredLectures)
+        for i,lecture in enumerate(StructuredLectures):
+            if len(lecture) == 3:
+                StructuredLectures[i].append("")
+            while len(lecture) > 5:
+                part = lecture[0:5]
+                del StructuredLectures[i][2:5]
+                StructuredLectures.insert(i, part)
+        TotalWeekList.append(StructuredLectures)
+    return(TotalWeekList)
+        
+
+
+        # 5 rätt
         #* Att göra  
         # Dela upp dem som består av två ex SVESVE och SVASVA i två stycken.
         # Ta bort repetering alltså SVESVE...
@@ -110,5 +115,5 @@ with sync_playwright() as playwright:
     insert(page, info = r"placeholder='Klass'",text="TE V2201")
     insert_and_choose_in_list(page, info = r"placeholder='Vecka'",text=str(startWeek), list_info="w-menu-item")
     page.wait_for_timeout(100)
-    scrape_calendar(page, dir)
+    print(scrape_calendar(page, dir))
     page.close()
